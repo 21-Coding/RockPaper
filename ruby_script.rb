0a35d25a1c5467af72e06c5f717bf33ed029a432
Student <example@example.com>
2019-12-11 11:10:35 -0800

    update scripts to hide input until winner is announced, then display input after.

--- ruby_script.rb
+++ ruby_script.rb
@@ -1,14 +1,19 @@
 #!/usr/bin/ruby
 
 require ('./lib/ruby_logic.rb')
+require 'io/console'
 
-puts "It's your turn, Player 1."
-player_one = gets.chomp.downcase()
-puts "It's your turn, Player 2."
-player_two = gets.chomp.downcase()
+puts "It's your turn, Player 1. (Enter rock, paper or scissors.)"
+player_one = STDIN.noecho(&:gets).chomp.downcase()
+puts "It's your turn, Player 2. (Enter rock, paper or scissors.)"
+player_two = STDIN.noecho(&:gets).chomp.downcase()
 game = RPS.new(player_one, player_two)
 if player_one == player_two
   puts game.tie?
+  puts "Player 1: #{player_one}"
+  puts "Player 2: #{player_two}"
 else
   puts game.win?
+  puts "Player 1: #{player_one}"
+  puts "Player 2: #{player_two}"
 end
